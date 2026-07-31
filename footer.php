96c43ad16f46fead57622f48ba236115ba072a93
Syeda-Zainab1 <syedazainabmubarak@gmail.com>
2026-07-31 20:49:14 +0500

    feat: update footer.php with additional widget area structure

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 <footer class="footer text-center py-2 theme-bg-dark">
 		   
             <p class="copyright"><a href="https://github.com/Syeda-Zainab1">FollowSyedaZainab</a></p>
-		   
+		    <?php
+				dynamic_sidebar('footer-1');
+			?> 
 	    </footer>
     
     </div>
